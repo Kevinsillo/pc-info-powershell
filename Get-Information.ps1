a4cc1c834caa46e65b643d975a251642805033b0
Killcab <lana_wifly_1992@hotmail.com>
2018-11-26 12:16:48 +0000

    [U] Optimization, attach, domain and owner

--- Get-Information.ps1
+++ Get-Information.ps1
@@ -27,71 +27,71 @@ $port = ''
 # -------------
 # - Variables -
 # -------------
-$ip = Get-WmiObject Win32_NetworkAdapterConfiguration | Where { $_.Ipaddress.length -gt 1 }
-$ip = $ip.ipaddress[0]
-$core = Get-CimInstance -ClassName Win32_Processor | Select-Object Name
-$core = $core.name
-$ram = Get-Ciminstance Win32_OperatingSystem | Select-Object @{Name = "total";Expression = {[int]($_.TotalVisibleMemorySize/1mb)}}
-$ram = $ram.total
-$domain = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object Domain
-$domain = $domain.domain
-$client = read-host "Client name?"
-$file="$client - $env:COMPUTERNAME.txt"
+$ip = (Get-WmiObject Win32_NetworkAdapterConfiguration | Where { $_.Ipaddress.length -gt 1 }).ipaddress[0]
+$core = (Get-CimInstance -ClassName Win32_Processor).Name
+$ram = (Get-Ciminstance Win32_OperatingSystem | Select-Object @{Name = "total";Expression = {[int]($_.TotalVisibleMemorySize/1mb)}}).total
+$domainName = (Get-CimInstance -ClassName Win32_ComputerSystem).Domain
+$domain = (Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain
+$company = read-host "Company name?"
+$owner = read-host "Owner name?"
+$file="$company - $owner - $env:COMPUTERNAME.txt"
 # --------------
 # - Out format -
 # --------------
 ECHO "-----------------------------------------------------------------------" > $file
 ECHO "COMPUTER INFORMATION - $((get-date).tostring('dd-MM-yyyy'))" >> $file
 ECHO "-----------------------------------------------------------------------" >> $file
-ECHO "CLIENT: $client" >> $file
+ECHO "COMPANY: $company" >> $file
 ECHO "COMPUTER NAME: $env:COMPUTERNAME" >> $file
 ECHO "CURRENT USER: $env:USERNAME" >> $file
 ECHO "CURRENT USER FOLDER: $env:USERPROFILE" >> $file
 ECHO "IP ADDRESS: $ip" >> $file
-ECHO "DOMAIN/WORKGROUP: $domain" >> $file
+if ($domain -eq 'true') {
+    ECHO "DOMAIN: $domainName"  >> $file
+} else {
+    ECHO "WORKGROUP: $domainName" >> $file
+}
 ECHO "PROCESSOR: $core" >> $file
-ECHO "MEMORY RAM: $ram GB" >> $file
+ECHO "MEMORY: $ram" >> $file
 ECHO "" >> $file
 Get-Wmiobject win32_logicaldisk -Filter "DriveType=3" | Select @{name="Unit";Expression={$_.Name}},@{name="Format";Expression={$_.FileSystem}},@{name="Name";Expression={$_.VolumeName}},@{n="Free Space";e={[math]::truncate($_.freespace / 1GB)}} >> $file
 if ($emailaddress -eq 'true') {
     ECHO "-----------------------------------------------------------------------" >> $file
     ECHO "EMAIL ADDRESS" >> $file
     ECHO "-----------------------------------------------------------------------" >> $file
-    $outlookApplication = New-Object -ComObject 'Outlook.Application'
-    $outlook = $outlookApplication.Application.Session.Accounts | Select-Object DisplayName
-    ECHO $outlook.DisplayName >> $file
+    $outlook = ((New-Object -ComObject 'Outlook.Application').Application.Session.Accounts | Select-Object DisplayName).DisplayName
+    ECHO $outlook >> $file
 }
 if ($sharedfolders -eq 'true') {
     ECHO "" >> $file
     ECHO "-----------------------------------------------------------------------" >> $file
     ECHO "SHARED FOLDERS" >> $file
     ECHO "-----------------------------------------------------------------------" >> $file
-    $sharedfolders = get-WmiObject -class Win32_Share | where {$_.path -ne ""}
-    ECHO $sharedfolders.path >> $file
+    $sharedfolders = (get-WmiObject -class Win32_Share | where {$_.path -ne ""}).path
+    ECHO $sharedfolders >> $file
 }
 if ($users -eq 'true') {
     ECHO "" >> $file
     ECHO "-----------------------------------------------------------------------" >> $file
     ECHO "ACTIVE LOCAL USERS" >> $file
     ECHO "-----------------------------------------------------------------------" >> $file
-    $users = Get-Localuser | where {$_.enabled -eq "true"}
-    ECHO $users.name >> $file
+    $users = (Get-Localuser | where {$_.enabled -eq "true"}).name
+    ECHO $users >> $file
 }
 if ($groups -eq 'true') {
     ECHO "" >> $file
     ECHO "-----------------------------------------------------------------------" >> $file
     ECHO "LOCAL GROUPS" >> $file
     ECHO "-----------------------------------------------------------------------" >> $file
-    $groups = Get-Localgroup
-    ECHO $groups.name >> $file
+    $groups = (Get-Localgroup).name
+    ECHO $groups >> $file
 }
 # --------------
 # - Send email -
 # --------------
 $send = read-host "Send mail with summary? [Y/N]"
 if ($send -eq 'Y') {
-    $subject = "$client - User:$env:USERNAME PC:$env:COMPUTERNAME"
-    $body = Get-Content $file -Encoding 'UTF8' | Out-String
-    $credencial = Get-Credential
-    Send-MailMessage -From $from -To $to -Subject $subject -Body $body -SmtpServer $smtp -Port $port -Encoding 'UTF8' -Credential $credencial
+    $subject = "$company - $owner - $env:COMPUTERNAME"
+    $credencial = Get-Credential -UserName $from
+    Send-MailMessage -From $from -To $to -Subject $subject -Attachments $file -SmtpServer $smtp -Port $port -Encoding 'UTF8' -Credential $credencial
 }

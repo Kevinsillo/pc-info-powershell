7cb67da1faab13048fbf1ae819230a91a7f97754
Killcab <lana_wifly_1992@hotmail.com>
2019-03-08 07:36:38 +0000

    Update GetInformationPowershell.ps1

--- GetInformationPowershell.ps1
+++ GetInformationPowershell.ps1
@@ -4,11 +4,11 @@
 #   DESCIPTION: Collect information from the computer          #
 #   and send it by mail.                                       #
 #                                                              #
-#   VERSION: 5.0                                               #
+#   VERSION: 1.0.6.0                                           #
 #                                                              #
 ################################################################
 ECHO OFF
-CLS
+Clear-Host
 # -----------
 # - Options -
 # -----------
@@ -37,65 +37,71 @@ $company = read-host "Company name?"
 $owner = read-host "Owner name?"
 $send = read-host "Send mail with summary? [Y/N]"
 $ip = (Get-WmiObject Win32_NetworkAdapterConfiguration | Where { $_.Ipaddress.length -gt 1 }).ipaddress[0]
+$mac = (Get-NetAdapter | Where-Object {$_.Name -eq 'Ethernet'}).MacAddress
+$adapterName = (Get-NetAdapter | Where-Object {$_.Name -eq 'Ethernet'}).Name
+$adapterDesc = (Get-NetAdapter | Where-Object {$_.Name -eq 'Ethernet'}).InterfaceDescription
 $core = (Get-CimInstance -ClassName Win32_Processor).Name
 $ram = (Get-Ciminstance Win32_OperatingSystem | Select-Object @{Name = "total";Expression = {[int]($_.TotalVisibleMemorySize/1mb)}}).total
 $domainName = (Get-CimInstance -ClassName Win32_ComputerSystem).Domain
 $domain = (Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain
 $os = (Get-CimInstance -ClassName Win32_OperatingSystem).caption
+$graphic = (Get-WmiObject Win32_VideoController).description
 $file = "$company - $owner - $env:COMPUTERNAME.txt"
 # --------------
 # - Out format -
 # --------------
-ECHO "-----------------------------------------------------------------------" > $file
-ECHO "COMPUTER INFORMATION" >> $file
-ECHO "By Kevin Illanas - 2018" >> $file
-ECHO "-----------------------------------------------------------------------" >> $file
-ECHO "DATE: $((get-date).tostring('dd-MM-yyyy'))" >> $file
-ECHO "COMPANY: $company" >> $file
-ECHO "COMPUTER NAME: $env:COMPUTERNAME" >> $file
-ECHO "OPERATING SYSTEM: $os" >> $file
+Write-Output "-----------------------------------------------------------------------" > $file
+Write-Output "COMPUTER INFORMATION" >> $file
+Write-Output "By Kevin Illanas - 2018" >> $file
+Write-Output "-----------------------------------------------------------------------" >> $file
+Write-Output "DATE: $((get-date).tostring('dd-MM-yyyy'))" >> $file
+Write-Output "COMPANY: $company" >> $file
+Write-Output "COMPUTER NAME: $env:COMPUTERNAME" >> $file
+Write-Output "OPERATING SYSTEM: $os" >> $file
 if ($domain -eq 'true') {
-    ECHO "DOMAIN: $domainName"  >> $file
+    Write-Output "DOMAIN: $domainName"  >> $file
 } else {
-    ECHO "WORKGROUP: $domainName" >> $file
+    Write-Output "WORKGROUP: $domainName" >> $file
 }
-ECHO "CURRENT USER: $env:USERNAME" >> $file
-ECHO "CURRENT USER FOLDER: $env:USERPROFILE" >> $file
-ECHO "IP ADDRESS: $ip" >> $file
-ECHO "PROCESSOR: $core" >> $file
-ECHO "MEMORY: $ram" >> $file
-ECHO "" >> $file
+Write-Output "CURRENT USER: $env:USERNAME" >> $file
+Write-Output "CURRENT USER FOLDER: $env:USERPROFILE" >> $file
+Write-Output "IP ADDRESS: $ip" >> $file
+Write-Output "MAC: $mac ($adapterName - $adapterDesc)" >> $file
+Write-Output "PROCESSOR: $core" >> $file
+Write-Output "MEMORY: $ram GB" >> $file
+Write-Output "GRAPHIC: $graphic" >> $file
+Write-Output "" >> $file
 Get-Wmiobject win32_logicaldisk -Filter "DriveType=3" | Select @{name="Unit";Expression={$_.Name}},@{name="Format";Expression={$_.FileSystem}},@{name="Name";Expression={$_.VolumeName}},@{n="Free Space";e={[math]::truncate($_.freespace / 1GB)}} >> $file
 if ($emailaddress -eq 'true') {
-    ECHO "-----------------------------------------------------------------------" >> $file
-    ECHO "EMAIL ADDRESS" >> $file
-    ECHO "-----------------------------------------------------------------------" >> $file
+    Write-Output "-----------------------------------------------------------------------" >> $file
+    Write-Output "EMAIL ADDRESS" >> $file
+    Write-Output "-----------------------------------------------------------------------" >> $file
     $outlook = ((New-Object -ComObject 'Outlook.Application').Application.Session.Accounts | Select-Object DisplayName).DisplayName
-    ECHO $outlook >> $file
+    Write-Output $outlook >> $file
 }
 if ($sharedfolders -eq 'true') {
-    ECHO "" >> $file
-    ECHO "-----------------------------------------------------------------------" >> $file
-    ECHO "SHARED FOLDERS" >> $file
-    ECHO "-----------------------------------------------------------------------" >> $file
+    Write-Output "" >> $file
+    Write-Output "-----------------------------------------------------------------------" >> $file
+    Write-Output "SHARED FOLDERS" >> $file
+    Write-Output "-----------------------------------------------------------------------" >> $file
     $sharedfolders = (get-WmiObject -class Win32_Share | where {$_.path -ne ""}).path
-    ECHO $sharedfolders >> $file
+    Write-Output $sharedfolders >> $file
 }
 if ($users -eq 'true') {
-    ECHO "" >> $file
-    ECHO "-----------------------------------------------------------------------" >> $file
-    ECHO "ACTIVE LOCAL USERS" >> $file
-    ECHO "-----------------------------------------------------------------------" >> $file
+    Write-Output "" >> $file
+    Write-Output "-----------------------------------------------------------------------" >> $file
+    Write-Output "ACTIVE LOCAL USERS" >> $file
+    Write-Output "-----------------------------------------------------------------------" >> $file
     $users = (Get-Localuser | where {$_.enabled -eq "true"}).name
-    ECHO $users >> $file
+    Write-Output $users >> $file
 }
 if ($groups -eq 'true') {
-    ECHO "" >> $file
-    ECHO "-----------------------------------------------------------------------" >> $file
-    ECHO "LOCAL GROUPS" >> $file
-    ECHO "-----------------------------------------------------------------------" >> $file
+    Write-Output "" >> $file
+    Write-Output "-----------------------------------------------------------------------" >> $file
+    Write-Output "LOCAL GROUPS" >> $file
+    Write-Output "-----------------------------------------------------------------------" >> $file
     $groups = (Get-Localgroup).name
-    ECHO $groups >> $file
+    Write-Output $groups >> $file
 }
 # --------------
 # - Send email -
